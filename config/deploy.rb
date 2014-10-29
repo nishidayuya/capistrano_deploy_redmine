181656a3064782ab3acb21278f95b9d2fa26c9cc
Yuya.Nishida <yuya@j96.org>
2014-10-29 21:22:15 +0900

    add "public/plugin_assets" to :linked_dirs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,16 @@ set :linked_files, %w{config/database.yml config/configuration.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{
+  bin
+  log
+  tmp/pids
+  tmp/cache
+  tmp/sockets
+  vendor/bundle
+  public/system
+  public/plugin_assets
+}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
